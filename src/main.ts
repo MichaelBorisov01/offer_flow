a7af71ff2d1cafca45b5171df8a3ca210016b36a
MichaelBorisov01 <bori.mix@yandex.ru>
2025-10-15 16:31:07 +0300

    улучшил стиль scrollbar

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'ant-design-vue/dist/reset.css'
+import './assets/styles/scroll.scss'
 
 const pinia = createPinia()
 const app = createApp(App)
